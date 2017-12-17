c77b00240addff1cc29296dadf6dfbb8dc292a9f
Amy <az287@cornell.edu>
2017-12-17 13:42:08 -0500

    Added files

--- _build/board.mli
+++
@@ -1,9 +0,0 @@
-open Graphics
-open State
-
-(* [draw_board st] takes in a state, the x position of the grid, and the y
- * position of the grid and draws a 4x4 board with the values
- * provided by [st]. Drawing the board may involve generating new tiles,
- * changing the position of tiles, and changing the values of current tiles
- * when tiles are merged and moved around on the board. *)
-val draw_board: int -> int -> State.state -> unit
